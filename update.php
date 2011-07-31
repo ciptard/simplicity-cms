02e906583421a729a9e8c514953902199524ba9a
shedali <shedali@gmail.com>
2011-07-31 16:19:56 +0100

    removes pass

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysql_connect("localhost", "shedali_vet", "7ea78je4") or die ('database wont connect because:'. mysql_error());
+$conn=mysql_connect("localhost", "your*username", "your*password") or die ('database wont connect because:'. mysql_error());
 $db = mysql_select_db ("shedali_vet_cms", $conn) or die("Couldn't open database");
 
 $contentValue = $_POST['contentValue'];
